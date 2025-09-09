34f165f2c8b752093da4afe61a0fd106eda63e18
holdenspete <78021639+holdenspete@users.noreply.github.com>
2025-09-09 13:22:12 +0100

    Dave's amends

--- wp-content/themes/holdens/header.php
+++ wp-content/themes/holdens/header.php
@@ -6,7 +6,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
+    <meta property="og:image" content="https://evc.holdens.space/img/socials-2.png" />
     <link rel="shortcut icon" href="/favicon.ico" />
     <link rel="icon" type="image/x-icon" href="/favicon.ico" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
