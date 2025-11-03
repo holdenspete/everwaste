dbc53e23d603509caf3d736e299b596b9c181ee2
holdenspete <78021639+holdenspete@users.noreply.github.com>
2025-11-03 08:58:47 +0000

    404

--- wp-content/themes/holdens/404.php
+++ wp-content/themes/holdens/404.php
@@ -30,7 +30,7 @@ h1 {
 
     <section class="page-hero">
         <div class="container">
-            <div class="page-hero-content">
+            <div class="page-hero-content" style="padding-top: 5rem;">
                 <h1 style="opacity:0;" class="page-title">404 Page Not Found</h1>
             </div>
         </div>
@@ -45,14 +45,9 @@ h1 {
 
 
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/SplitText.min.js"></script>
 <script>
 document.addEventListener('DOMContentLoaded', function() {
-        gsap.registerPlugin(SplitText) 
 
         // GSAP timeline for .reveal
         if (window.gsap) {
@@ -61,19 +56,8 @@ document.addEventListener('DOMContentLoaded', function() {
             
             const h1 = document.querySelector('.page-hero h1');
             
-            if (h1 && window.SplitText) {
-                const split = new SplitText([h1], { type: "words,chars" });
-                gsap.set(split.chars, { y: 80, opacity: 0 });
-                gsap.set(h1, { opacity: 1 });
-                tl.to(split.chars, {
-                    y: 0,
-                    opacity: 1,
-                    duration: 0.6,
-                    stagger: 0.03,
-                    ease: "power3.out"
-                });
-            }
-            tl.to('.page-content', { y: 0, opacity: 1, duration: .5 }, "-=0.65");
+            tl.to(h1, { y: 0, opacity: 1, duration: .5 });
+            tl.to('.page-content', { y: 0, opacity: 1, duration: .5 }, "-=0.25");
 
         }
 
